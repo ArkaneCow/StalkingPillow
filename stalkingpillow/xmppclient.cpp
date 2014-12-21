bc17f6282265b11524ac2280da9dc3727c5000e7
Jason Wu <jasonwupilly1997@gmail.com>
2014-12-21 13:05:03 -0500

    begin event handling

--- stalkingpillow/xmppclient.cpp
+++ stalkingpillow/xmppclient.cpp
@@ -30,4 +30,17 @@ void xmppClient::presenceChanged(const QString& bareJid, const QString& resource
     QDateTime current = QDateTime::currentDateTime();
     qDebug(qPrintable(current.toString()));
     qDebug("Presence changed %s/%s", qPrintable(bareJid), qPrintable(resource));
+    QSettings settings;
+    QString monitorList = settings.value("facebook/monitor").toString();
+    QStringList monitor = monitorList.split(",");
+    QStringList list;
+    for (int i = 0; i < monitor.size(); i++) {
+        if (!monitor.at(i).contains("|")) {
+            QStringList splitName = monitor.at(i).split("|");
+            QString userID = splitName.at(0);
+            if (userID.compare(bareJid) == 0) {
+
+            }
+        }
+    }
 }
